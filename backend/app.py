ea8721547d31a22824a99a2c6205808d7d24d469
Laura <laura.e.vollmer@gmail.com>
2025-03-13 16:54:36 -0500

    Connecting the backend and frontend for development and testing

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,26 @@
+# Main entry point for Flask backend
+# intializes the app
+# Connects the databases
+# Imports the routes
+import os
+from flask import send_from_directory
 import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+# Cross-origin requests from React app
+CORS(app)
 
 def hello_world():
     return 'Hello world!'
+
+@app.route('/')
+def serve_react_app():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'build'), 'index.html')
+
+@app.route('/<path:path>')
+def serve_react_static(path):
+    return send_from_directory(os.path.join(app.root_path, 'static', 'build'), path)
 
 if __name__ == '__main__':
     app.run(debug=True)
