e042af5f8f1da2fa64580f6a510cfbcf549d82a9
Laura <laura.e.vollmer@gmail.com>
2025-03-13 19:03:01 -0500

    Updating the practice page

--- frontend/src/pages/practice.js
+++ frontend/src/pages/practice.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const practice = () => {
-  return (
-    <div>practice</div>
-  )
+    const [name, setName] = useState("");
+
+    const handleSubmit= (e) => {
+        e.preventDefault();
+        alert(`The medication you entered was ${name}`)
+    }
+    return (
+        <form>
+            <div>
+                <label>Enter medication name:
+                    <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}></input>
+                </label>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    )
 }
 
 export default practice
