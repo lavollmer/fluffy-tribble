14fc83c6555fa67ddfb8c2b8c6915ad2d623e493
Laura <laura.e.vollmer@gmail.com>
2025-03-17 14:32:14 -0500

    Downloaded React Icons for hamburger icon for header and designed header based on Figma

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const header = () => {
   return (
-    <div>
-      <div>
-        <h1 className='font-roboto text-lg bg-blue-500'>MEDPRONUNCIATION</h1>
+    <div className='m-10 flex flex-row justify-between items-center'>
+      <h1 className='font-roboto text-2xl font-bold'>MEDPRONUNCIATION</h1>
+      <div className='flex flex-row justify-center items-center space-x-10 text-lg'>
+        <h2>About</h2>
+        <h2>Practice</h2>
+        <RxHamburgerMenu />
       </div>
     </div>
   )
