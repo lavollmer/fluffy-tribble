f183e4d9a1b4b56971d8415c625bd76c89bc9f32
Laura <laura.e.vollmer@gmail.com>
2025-03-18 11:08:17 -0500

    Learning how to make a Star Rating

--- frontend/src/components/starrating.jsx
+++ frontend/src/components/starrating.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaRegStar } from "react-icons/fa";
 
-const starrating = () => {
+const starrating = ({totalStars = 5}) => {
+    const [star, setStar] = useState(0);
+    const [hover, setHover] = useState(0);
+    
     return (
         <>
             <div className='flex flex-col items-center justify-center'>
@@ -9,12 +12,16 @@ const starrating = () => {
                     <h1 className='text-2xl font-bold'>Rate your MedPronuncation</h1>
                 </div>
                 <div className='flex flex-row space-x-2 p-4'>
-                    <FaRegStar style={{ fontSize: '2em' }} />
+                    <FaRegStar style={{ color: 'text-gray-400', fontSize: '2em' }} />
                     <FaRegStar style={{ fontSize: '2em' }} />
                     <FaRegStar style={{ fontSize: '2em' }} />
                     <FaRegStar style={{ fontSize: '2em' }} />
                     <FaRegStar style={{ fontSize: '2em' }} />
                 </div>
+                <div>
+                    <h1>Based on XX reviews</h1>
+                </div>
+                <button className='bg-custom-primary text-white font-bold p-4 rounded-lg'>Submit</button>
             </div>
         </>
     )
