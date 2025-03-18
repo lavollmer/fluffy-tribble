6a97c1f1630cccbbfdec29bef4ee4872cbe42f75
Laura <laura.e.vollmer@gmail.com>
2025-03-18 10:33:52 -0500

    Created an assets folder and imported main image from unsplash

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx'
+import mainImage from "../assets/mainimage.jpg"
 
 const home = () => {
   return (
     <div>
-        <Header />
-        <Footer />
+      <Header />
+      <img src={mainImage} alt="mainImage" />
+      <Footer />
     </div>
   )
 }
