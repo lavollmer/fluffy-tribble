82fc071555f06044ded3dcc955c58d554f582809
Laura <laura.e.vollmer@gmail.com>
2025-03-18 10:21:43 -0500

    Creating footer section with logo, links and copyright statement

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div className='flex flex-row justify-between items-center font-roboto bg-blue-500'>
-      <div>
-      <h1 className='text-2xl font-bold'>MEDPRONUNCIATION</h1>
+    <>
+      <div className='flex flex-row items-center justify-center font-roboto'>
+        <div className='grid grid-cols-2 gap-4 p-4'>
+          <div>
+            <h1 className='text-2xl font-bold'>MEDPRONUNCIATION</h1>
+          </div>
+          <div className='flex flex-row items-center font-bold justify-evenly'>
+            <h1>ABOUT</h1>
+            <h1>PRACTICE</h1>
+            <h1>FEEDBACK</h1>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className='flex flex-row items-center justify-center font-roboto'>
+        <div>&copy;{new Date().getFullYear()} Laura Dev</div>
+      </div>
+    </>
   )
 }
 
