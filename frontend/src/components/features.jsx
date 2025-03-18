8b848d2389f2bb36e83ababa0f1f545813e21a50
Laura <laura.e.vollmer@gmail.com>
2025-03-18 11:44:20 -0500

    Updating the styling on the features section

--- frontend/src/components/features.jsx
+++ frontend/src/components/features.jsx
@@ -3,7 +3,7 @@ import TextCard from './textcard.jsx'
 
 const features = () => {
     return (
-        <div className='flex flex-col items-center justify-center p-6'>
+        <div className='flex flex-col items-center justify-center p-6 pt-10 font-roboto'>
             <h1 className='text-2xl font-bold'>FEATURES</h1>
             <div className='flex flex-row items-center justify-center p-6 space-x-10'>
                 <TextCard title="Accurate Pronunciation of Medical Terms" text="Our platform offers healthcare professionals a comprehensive guide to correctly pronounce complex medical terminology. With phonetic breakdowns, audio guides, and easy-to-follow examples, you'll master the pronunciation of anatomical terms, disease names, and medical procedures. Whether you're in nursing, pharmacy, or medicine, our resources ensure you're confident and precise in your communications." hashtagOne="MedicalPronunciation " />
