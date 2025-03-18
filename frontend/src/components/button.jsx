a40d06dbb59a6eabc88285aa128dbac022be6425
Laura <laura.e.vollmer@gmail.com>
2025-03-18 10:37:25 -0500

    Creating the main button and home page with Figma based designs

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const button = () => {
+const button = ({buttonText}) => {
   return (
     <div className='flex flex-row items-center justify-center px-6 py-4 rounded-full'>
       <div className='bg-black text-custom-primary'>{buttonText}</div>
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx'
 import mainImage from "../assets/mainimage.jpg"
+import Button from "../components/button.jsx"
 
 const home = () => {
   return (
     <div>
       <Header />
-      <img src={mainImage} alt="mainImage" />
+      <div className='grid grid-cols-2 font-roboto'>
+        <div className='flex flex-col items-center justify-center p-6'>
+          <div className='flex flex-col items-center justify-center space-y-4'>
+            <h1 className='font-bold text-4xl'>Saying a medication name doesn't have to be hard.</h1>
+            <h2>MedPronuncation works to teach healthcare professionals to pronounce medical terminology.</h2>
+          </div>
+          <div>
+            <Button buttonText='Start Today' />
+          </div>
+        </div>
+        <div>
+          <img src={mainImage} alt="mainImage" />
+        </div>
+      </div>
       <Footer />
     </div>
   )
