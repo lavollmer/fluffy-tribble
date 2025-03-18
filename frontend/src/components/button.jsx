880a7caa35ec957f114354610484c44c6c089659
Laura <laura.e.vollmer@gmail.com>
2025-03-18 10:30:54 -0500

    Created a custom reusable button in components for application

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const button = () => {
   return (
-    <div>button</div>
+    <div className='flex flex-row items-center justify-center px-6 py-4 rounded-full'>
+      <div className='bg-black text-custom-primary'>{buttonText}</div>
+    </div>
   )
 }
 
