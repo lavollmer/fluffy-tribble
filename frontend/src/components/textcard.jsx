1190f501f9e8e0dbc2443cd234dad2469f75f85a
Laura <laura.e.vollmer@gmail.com>
2025-03-18 11:39:26 -0500

    Adding SEO friendly and copy to the features section, also added three unique hashtag options

--- frontend/src/components/textcard.jsx
+++ frontend/src/components/textcard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const textcard = ({text,title,hashtag}) => {
+const textcard = ({ text, title, hashtagOne, hashtagTwo, hashtagThree }) => {
     return (
         <>
             <div className='max-w-sm rounded overflow-hidden shadow-lg'>
@@ -12,7 +12,9 @@ const textcard = ({text,title,hashtag}) => {
                     <p className='text-gray-700 text-base'>{text}</p>
                 </div>
                 <div>
-                    <p className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>#{hashtag}</p>
+                    <p className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>#{hashtagOne}</p>
+                    <p className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>#{hashtagTwo}</p>
+                    <p className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>#{hashtagThree}</p>
                 </div>
             </div>
         </>
