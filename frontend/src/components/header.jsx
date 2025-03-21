11ddd0fa385a33ace7b1f3965dc4b25827060511
Laura <laura.e.vollmer@gmail.com>
2025-03-21 17:55:08 -0500

    Adding signup button

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -17,17 +17,26 @@ const header = () => {
         <div className='flex flex-row justify-center items-center m-10'>
           <h1 className='text-3xl font-bold cursor-pointer hover:text-custom-bright-orange'>
             <Link to="/">MEDPRONUNCIATION AI</Link>
-            </h1>
+          </h1>
+        </div>
+        <div className='flex flex-row items-center justify-center text-xl m-10 space-x-10'>
+          <h2 className='text-xl'>New to MedPronunciation AI? </h2>
+          <div className='text-custom-bright-orange'>
+            <Link to="/signup">Sign Up Free</Link>
+          </div>
         </div>
         <div className='m-10 flex flex-row justify-center items-center space-x-10 text-xl'>
           <div className='flex flex-row justify-center items-center space-x-10'>
             <div className='hover:text-custom-bright-orange'>
               <Link to="/login">Login</Link>
-            </div><div className='hover:text-custom-bright-orange'>
+            </div>
+            <div className='hover:text-custom-bright-orange'>
               <Link to="/practice">Practice</Link>
-            </div><div className='hover:text-custom-bright-orange'>
+            </div>
+            <div className='hover:text-custom-bright-orange'>
               <Link to="/contact">Contact Us</Link>
-            </div><div className='hover:text-custom-bright-orange'>
+            </div>
+            <div className='hover:text-custom-bright-orange'>
               <Link to="/about">About</Link>
             </div>
           </div>
--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -6,10 +6,10 @@ const signup = () => {
         <div className='flex flex-col items-center justify-center space-y-8 font-roboto p-20'>
             <div className='flex flex-col items-center justify-center'>
                 <h1 className='font-italic text-2xl font-bold'>Ready to become an expert on medical terminology?</h1>
-                <p className='text-lg'>Sign up to get started!</p>
+                <p className='text-lg'>Sign up to get started for free!</p>
             </div>
             <div className='flex flex-col items-center justify-center space-y-4 bg-custom-lighter-blue p-4 rounded-lg shadow-lg p-10'>
-                <h2 className='font-bold text-xl'>Sign Up</h2>
+                <h2 className='font-bold text-xl'>SIGN UP FREE</h2>
                 <form className='flex flex-col justify-center items-center mt-8 mb-2 w-80 max-w-screen-lg sm:w-96'>
                     <div className='flex flex-col items-center justify-center space-y-4 bg-grey-100 p-4 rounded-lg'>
                         <label for='name'className='font-bold text-lg'>Full Name</label>
@@ -28,7 +28,7 @@ const signup = () => {
                         <input type='text' id='profession' placeholder='Enter your profession' className='border-2 border-black rounded-lg bg-gray-100 px-4 py-2' />
                     </div> */}
                     <div>
-                        <button type="submit" className='bg-black text-custom-primary px-8 py-2 rounded-lg shadow-lg text-md hover:bg-custom-light-blue hover:cursor-pointer'>
+                        <button type="submit" className='bg-black text-custom-primary px-8 py-2 rounded-lg shadow-lg text-md hover:bg-grey-700 hover:cursor-pointer'>
                             Sign Up
                         </button>
                     </div>
