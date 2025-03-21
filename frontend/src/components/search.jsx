2017afc8324097d6b48a98475a941c6a8c44caee
Laura <laura.e.vollmer@gmail.com>
2025-03-21 14:02:07 -0500

    Adding a search bar

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const search = () => {
-  return (
-    <div>
-        <form>
-            <label for="search">Search</label>
-            <input type="text" id="search" name="search" />
-            <button type='submit'>Search</button>
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            <form className='flex items-center'>
+                <label for="search" className='sr-only'>Search</label>
+                <div className='relative w-full'>
+                    <input type="text" id="search" name="search" className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-bright-orange block w-full pl-10 p-2.5' placeholder='Search medication names, classes, etc' required />
+                </div>
+                <button type='submit' className='inline-flex items-center py-2.5 px-3 ml-2 text-sm font-md text-white bg-custom-rose rounded-lg border border-black'>Search</button>
+            </form>
+        </div>
+    )
 }
 
 export default search
--- frontend/src/pages/practice.jsx
+++ frontend/src/pages/practice.jsx
@@ -3,15 +3,20 @@ import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx'
 import TTS from "../components/texttospeech.jsx"
 import StarRating from "../components/starrating.jsx"
+import Search from "../components/search.jsx"
 
 
 const practice = () => {
   return (
-    <div className='bg-custom-primary font-roboto'>
+    <div className='flex flex-col bg-custom-primary font-roboto space-y-4'> 
       <Header />
-      <div className='flex flex-col items-center justify-center space-y-4'>
+      <div className='flex flex-col justify-center items-center space-y-4'>
         <h1 className='font-bold text-4xl text-black'>Practice</h1>
         <p className='text-lg text-custom-grey'>Practice your pronunciation and listening skills</p>
+        <Search />
+      </div>
+      <div className='flex flex-col items-center justify-center space-y-4'>
+        <p className='text-lg text-custom-grey'>Type in the medication name you wish to pronounce</p>
         <TTS />
       </div>
       <div>
