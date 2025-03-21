d67da9df2f454c355487f203d1af38d9f6d6cce4
Laura <laura.e.vollmer@gmail.com>
2025-03-21 17:39:25 -0500

    Working on the Sign Up area

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -3,13 +3,14 @@ import Button from '../components/button.jsx'
 
 const signup = () => {
     return (
-        <div className='flex flex-col items-center justify-center space-y-4 font-roboto'>
+        <div className='flex flex-col items-center justify-center space-y-4 font-roboto p-20'>
             <div className='flex flex-col items-center justify-center space-y-4'>
                 <h1 className='font-italic text-2xl'>Ready to become an expert on medical terminology?</h1>
             </div>
-            <div className='flex flex-col items-center justify-center space-y-4'>
-                <form>
-                    <div>
+            <div className='flex flex-col items-center justify-center space-y-4 border-2 border-black p-4 rounded-lg shadow-lg'>
+                <h2 className='font-bold text-xl'>Sign Up</h2>
+                <form className='flex flex-col justify-center items-center md:flex-row md:space-x-4 mt-8 mb-2 w-80 max-w-screen-lg sm:w-96'>
+                    <div className='flex flex-col items-center justify-center space-y-4 bg-grey-100 p-4 rounded-lg shadow-lg'>
                         <label for='name'>Name</label>
                         <input type='text' id='name' placeholder='Enter your name' />
                     </div>
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import SignUp from '../components/signup'
 
 const login = () => {
   return (
-    <div>login</div>
+    <div>
+      <Header />
+      <SignUp />
+      <Footer />
+      </div>
   )
 }
 
