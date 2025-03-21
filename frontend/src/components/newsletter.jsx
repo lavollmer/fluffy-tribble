001a313f0383054d4e626f9d72718787c0d8550a
Laura <laura.e.vollmer@gmail.com>
2025-03-21 13:45:25 -0500

    Updated the favicon in the website

--- frontend/src/components/newsletter.jsx
+++ frontend/src/components/newsletter.jsx
@@ -32,7 +32,7 @@ const newsletter = () => {
     <div className="bg-black p-10 flex flex-col items-center justify-center text-center font-roboto">
       <div className="flex flex-col space-y-4 text-white">
         <h1 className="text-lg">BE ONE OF THE FIRST TO JOIN</h1>
-        <h2 className="text-2xl">Stay up-to-date with what Laura Dev is working on</h2>
+        <h2 className="text-xl">Stay up-to-date with what LAURA DEV is working on</h2>
       </div>
       <div className="flex flex-row justify-center items-center text-center w-full">
         {/* form input with onSubmit handleSubmit function */}
@@ -51,7 +51,7 @@ const newsletter = () => {
           {error && <p className="text-red-500 mt-2">{error}</p>}
           <button
             type="submit"
-            className="flex md:flex-row justify-center items-center text-center bg-soft-red font-bold text-white rounded-md text-md w-1/2 h-3/4 md:text-lg mt-4 md:w-1/2 md:h-full md:mt-0"
+            className="flex md:flex-row justify-center items-center text-center bg-custom-lighter-blue font-bold text-black rounded-md text-md w-1/2 h-3/4 md:text-lg mt-4 md:w-1/2 md:h-full md:mt-0 hover:bg-custom-light-blue"
           >
             Contact Us
           </button>
